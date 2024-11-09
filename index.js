f21207638a66be66dab3ccc3276de62c4bfddb95
Your Name <fahimrashid350@gmail.com>
2024-11-09 13:58:31 +0600

    review and find single user api created

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.maw05.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,6 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const usersCollection = client.db("serviceFinder").collection("users");
+    const reviewsCollection = client.db("serviceFinder").collection("reviews");
     const categoriesCollection = client
       .db("serviceFinder")
       .collection("categories");
@@ -47,6 +48,21 @@ async function run() {
 
     app.get("/categories", async (req, res) => {
       const result = await categoriesCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.findOne(query);
       res.send(result);
     });
 
